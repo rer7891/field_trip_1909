14b2640594b07d41bcb5534a5289d0af3ea93139
Becky Robran <rerobran@gmail.com>
2020-01-13 09:34:28 -0700

    Fixes errors in test setup

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,6 @@
 class Passenger < ApplicationRecord
-  validates_prsenence_of :name
-  validates_prsenence_of :age
+  validates_presence_of :name
+  validates_presence_of :age
 
   has_many :passenger_flights
   has_many :flights, through: :passenger_flights
