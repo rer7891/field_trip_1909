f8d79e49ae2de7d9d03d38bb37d228bd9867bf89
Becky Robran <rerobran@gmail.com>
2020-01-13 10:49:15 -0700

    adds route to add flight to passenger

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/flights/:id', to: 'flights#show'
 
   get '/passengers/:id', to: 'passengers#show'
+
+  post '/passengers/:id/flights/new', to: 'flights#new'
 end
