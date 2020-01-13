5fd7a3758326283ff2b9ced9943f87f0a5db045d
Becky Robran <rerobran@gmail.com>
2020-01-13 11:00:39 -0700

    Adds flight model testing for new methods

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe Flight, type: :model do
     it {should have_many :passenger_flights}
     it {should have_many(:passengers).through(:passenger_flights)}
   end
+
+  describe 'methods' do
+    it "can count adults and minors on a flight" do
+      @southwest = Airline.create(name: "Southwest")
+
+      @southwest_1 = @southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
+      @passenger_1 = Passenger.create(name: "Susie", age: 37)
+      @passenger_2 = Passenger.create(name: "Bill", age: 7)
+      @passenger_3 = Passenger.create(name: "Tom", age: 23)
+      @passenger_4 = Passenger.create(name: "Phil", age: 53)
+      @passenger_5 = Passenger.create(name: "Becky", age: 10)
+
+      @southwest_1.passengers << [@passenger_1, @passenger_2, @passenger_3, @passenger_4, @passenger_5]
+
+      expect(@southwest_1.adult_count).to eq(3)
+      expect(@southwest_1.minor_count).to eq(2)
+    end
+  end
 end
