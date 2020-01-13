a9a05d3436b20f4fca3073f3d975b9503a00864b
Becky Robran <rerobran@gmail.com>
2020-01-13 11:05:07 -0700

    Adds model methods

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,12 @@ class Flight <ApplicationRecord
   belongs_to :airline
   has_many :passenger_flights
   has_many :passengers, through: :passenger_flights
+
+  def adult_count
+    passengers.where("age >= 18").count
+  end
+
+  def minor_count
+    passengers.where("age < 18").count
+  end
 end
