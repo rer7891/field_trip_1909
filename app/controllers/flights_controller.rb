9f99ad1005152ef26b50485cbda74c49812223f4
Becky Robran <rerobran@gmail.com>
2020-01-13 10:49:58 -0700

    adds new action to flight controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,4 +3,13 @@ class FlightsController < ApplicationController
   def show
     @flight = Flight.find(params[:id])
   end
-end 
+
+  def new
+    passenger = Passenger.find(params[:id])
+    flight = Flight.where(number: params[:number]).first
+    passenger.flights << flight
+    redirect_to "/passengers/#{passenger.id}"
+    end
+  end
+  end
+end
