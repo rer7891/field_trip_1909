636fc2671d99aa4ef399156cb92cfcdb0a74c53a
Becky Robran <rerobran@gmail.com>
2020-01-13 09:15:52 -0700

    Updates flights with many to many relationship passengers

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight <ApplicationRecord
   validates_presence_of :number, :date, :time, :departure_city, :arrival_city
   belongs_to :airline
+  have_many :passenger_flights
+  have_many :passengers, through: :flights
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -10,5 +10,7 @@ RSpec.describe Flight, type: :model do
   end
   describe 'relationships' do
     it {should belong_to :airline}
+    it {should have_many :passenger_flights}
+    it {should have_many(:passengers).through(:passenger_flights)}
   end
 end
